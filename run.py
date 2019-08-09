c0ba8ec5073b87081d6ce193820767bfb133f453
Chika Kanu <45425346+ChikaKanu@users.noreply.github.com>
2019-08-09 20:49:59 +0100

    Chikamso

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    Items = ["Apples", "Pear", "Banana"]
+
+    return ""
+
+@app.route("/test")
+def test():
+    paragraph = "<p>Chikamso is my name 54321, Lift of! <em>The person in the world</em></p>"
+    return "<strong>Chikamso is my name</strong>" + paragraph
