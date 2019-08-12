3135555c232de89b7cdacf8e696e3a73c092b48c
Chika Kanu <45425346+ChikaKanu@users.noreply.github.com>
2019-08-12 22:15:27 +0100

    Stat

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    Items = ["Apples", "Pear", "Banana"]
-
-    return ""
+     Items = ["Apples", "Pear", "Banana"]
+     output = render_template("start.html", name="Chikamso Kanu", Items=Items)
+     print(output)
+     return output
 
 @app.route("/test")
 def test():
-    paragraph = "<p>Chikamso is my name 54321, Lift of! <em>The person in the world</em></p>"
-    return "<strong>Chikamso is my name</strong>" + paragraph
+    print(request)
+    return render_template("test.html", name="Chikamso Kanu")
