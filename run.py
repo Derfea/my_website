3b7c0fc2e5c41ccb3f384c5ab8774532e3ecfa53
Chika Kanu <45425346+ChikaKanu@users.noreply.github.com>
2019-08-13 16:18:54 +0100

    Brilliant

--- run.py
+++ run.py
@@ -2,15 +2,39 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+class Item():
+    def __init__(self, name, amounts):
+        self.name = name
+        self.amounts = amounts
+
 
 @app.route("/")
 def hello():
-     Items = ["Apples", "Pear", "Banana"]
-     output = render_template("start.html", name="Chikamso Kanu", Items=Items)
-     print(output)
-     return output
+
+    # Items = [
+    #     Item("Apples", 5),
+    #     Item("Computer", 1),
+    #     Item("Pear", 4)
+    # ]
+
+    Items = [
+        {"name": "Apples", "amounts": 5},
+        {"name": "Computer", "amounts": 1},
+        {"name": "Pear", "amounts": 4}
+    ]
+
+    for item in Items:
+        item["amounts"] = item["amounts"] * 2
+
+    person = ("Chikamso", "Kanu")
+
+    output = render_template("start.html", person=person , Items=Items)
+    return output
 
 @app.route("/test")
 def test():
-    print(request.args)
-    return render_template("test.html", name="Chikamso Kanu")
+    args =  request.args
+    age = args.get("age")
+    name = args.get("name")
+
+    return render_template("test.html", name = name, age = age)
